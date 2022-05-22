d66cc50faad144f9f476d18748239f1467f17e76
huzbasicamila <92604250+huzbasicamila@users.noreply.github.com>
2022-05-22 18:19:58 +0200

    init

--- app.js
+++ app.js
@@ -2,5 +2,17 @@ let search=document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = ()=> {
     search.classList.toggle('active');
-
 }
+
+let menu=document.querySelector('.menu-box');
+
+document.querySelector('#menu-icon').onclick = ()=> {
+    menu.classList.toggle('active');
+}
+
+//Header
+let header = document.querySelector('header');
+
+window.addEventListener('scroll' , ()=> {
+    header.classList.toggle('shadow', window.scrollY>0);
+})

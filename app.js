499ffd3691fa37490316b55d8ede734ec21f1006
huzbasicamila <92604250+huzbasicamila@users.noreply.github.com>
2022-05-22 18:27:05 +0200

    jjjj

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ let search=document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = ()=> {
     search.classList.toggle('active');
+    menu.classList.remove('active');
 }
 
-let menu=document.querySelector('.menu-box');
+let menu=document.querySelector('.navbar');
 
 document.querySelector('#menu-icon').onclick = ()=> {
     menu.classList.toggle('active');
+    search.classList.remove('active');
 }
 
 //Header

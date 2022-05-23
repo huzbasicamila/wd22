6821275832f7685f4e6671ef3508d5c4fd4ce373
huzbasicamila <92604250+huzbasicamila@users.noreply.github.com>
2022-05-23 02:59:34 +0200

    u pokusaju

--- app.js
+++ app.js
@@ -23,8 +23,36 @@ let header = document.querySelector('header');
 window.addEventListener('scroll' , ()=> {
     header.classList.toggle('shadow', window.scrollY>0);
 })
-
+//GET
 fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars')
     .then(response=> {
             console.log(response);
     })
+
+//POST
+
+const addFood = () => {
+    const AutoID=document.getElementById("auto-id").value;
+    
+
+fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars`, {
+    method: 'POST',
+    headers: new Headers({'content-type': 'application/json'}),
+    body: JSON.stringify({
+
+      id: AutoID,
+     
+    })
+  })
+  .then(responce => {
+    console.log(responce);
+  })
+} 
+const deleteCar = (a=-1) => {
+    fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars/-1`, {
+      method: 'DELETE'
+    })
+    .then(res => {
+      console.log(res);
+    })
+  }
